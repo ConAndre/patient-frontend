f859901ff2ab4bce3be5dd0b8fe7a544801163c3
Andre Hernandez <hernandredre@gmail.com>
2021-12-02 20:32:34 -0500

    cors is a function

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(express.static(__dirname + '/dist/patient-frontend'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/patient-frontend/index.html'));});
