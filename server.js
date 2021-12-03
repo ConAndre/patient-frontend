d8c46c2ab56532e730e5060cc0043e97e701b499
Andre Hernandez <hernandredre@gmail.com>
2021-12-02 20:04:38 -0500

    convert to es6

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
+import express, { static } from 'express';
+import { join } from 'path';
 const app = express();
-app.use(express.static(__dirname + '/dist/patient-frontend'));
+app.use(static(__dirname + '/dist/patient-frontend'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/patient-frontend/index.html'));});
+res.sendFile(join(__dirname+'/dist/patient-frontend/index.html'));});
 app.listen(process.env.PORT || 8080);
